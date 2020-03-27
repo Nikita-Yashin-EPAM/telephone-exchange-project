f396f0f08d03e1616d4cdada114b38f3dd479602
Nikita <forepamtrainingtestautomation@gmail.com>
2020-03-27 20:49:53 +0300

    POJOs implemented, test a workflow with Git Issues -> Closes #1.

---
+++ src/main/java/telephone/exchange/station/telephoneexchangestation/model/User.java
@@ -0,0 +1,22 @@
+package telephone.exchange.station.telephoneexchangestation.model;
+
+public class User {
+    private String name;
+    private double account;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getAccount() {
+        return account;
+    }
+
+    public void setAccount(double account) {
+        this.account = account;
+    }
+}
