81414882a993af5b3097e22907eed96bbe737637
Nikita <forepamtrainingtestautomation@gmail.com>
2020-03-27 20:26:17 +0300

    Initial commit

---
+++ src/main/java/telephone/exchange/station/telephoneexchangestation/TelephoneExchangeStationApplication.java
@@ -0,0 +1,13 @@
+package telephone.exchange.station.telephoneexchangestation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelephoneExchangeStationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelephoneExchangeStationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/telephone/exchange/station/telephoneexchangestation/TelephoneExchangeStationApplicationTests.java
@@ -0,0 +1,13 @@
+package telephone.exchange.station.telephoneexchangestation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelephoneExchangeStationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
